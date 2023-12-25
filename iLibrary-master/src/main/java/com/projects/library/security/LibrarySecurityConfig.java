f379b42c77556b2915162dc1849e24fb87f176a9
akhil <gollaakhil1115@gmail.com>
2023-12-24 23:04:24 -0600

    implemented SecurityFilterChain - to disable CSRF and provided restrictions to the URLs.

--- iLibrary-master/src/main/java/com/projects/library/security/LibrarySecurityConfig.java
+++ iLibrary-master/src/main/java/com/projects/library/security/LibrarySecurityConfig.java
@@ -2,13 +2,22 @@ package com.projects.library.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class LibrarySecurityConfig {
+
+    //secure all the URLS except the Unscured URls
+    private static final String[] SECURED_URLs = {"/books/**"};
+    private static final String[] UN_SECURED_URLs = {"/books/all",
+            "/books/book/{id}",
+            "/users/**"};
+
 
 
 //    1. encoding the password
@@ -16,6 +25,26 @@ public class LibrarySecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
+    }
+
+    //4.
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        //disable the CSRF
+        return http.csrf().disable()
+                //we have to specify endPoints what are allowed
+                .authorizeHttpRequests()
+                .requestMatchers(UN_SECURED_URLs).permitAll()
+                .and()
+                //  we have to specify endPoints what are allowed to Admin
+                .authorizeHttpRequests()
+                //we need to declare List of endpoints to secure by admin
+                .requestMatchers(SECURED_URLs)
+                .hasAuthority("ADMIN")
+                .anyRequest().authenticated()
+                .and().formLogin().and().build();
+
     }
 
 
