66ae7c88f3390c8acd7593c57025dd398b515e62
akhil <gollaakhil1115@gmail.com>
2023-12-24 22:50:03 -0600

    implemented UserDetailsService to return UserDetails

--- iLibrary-master/src/main/java/com/projects/library/security/LibraryUserDetailsService.java
+++ iLibrary-master/src/main/java/com/projects/library/security/LibraryUserDetailsService.java
@@ -1,4 +1,29 @@
 package com.projects.library.security;
 
-public class LibraryUserDetailsService {
+import com.projects.library.user.UserRepository;
+import com.projects.library.user.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+@Component
+public class LibraryUserDetailsService implements UserDetailsService {
+
+    //3. we need to load the User from Database
+
+//    private UserService userService;
+
+    //we can directly load from even from Repository
+    @Autowired
+    private UserRepository userRepository;
+
+    //here we need to return User Details, that is why we implemented LibraryUserDetails First
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByEmail(username) //here we just cant return Username, we nned to return the LibraryUserDetails( which implements IUserDetails)
+                .map(LibraryUserDetails:: new) // here if User doest not found, it will throw NullPointerException
+                .orElseThrow(()-> new UsernameNotFoundException("No User Found"));
+    }
 }
