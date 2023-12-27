6722325c5649959019d5537c10fcb1623fc5554c
akhilgolla05 <149541408+akhilgolla05@users.noreply.github.com>
2023-12-26 23:14:48 -0600

    Update JwtAuthenticationFilter.java
    added desc

--- iLibrary-master/src/main/java/com/projects/library/security/jwt/JwtAuthenticationFilter.java
+++ iLibrary-master/src/main/java/com/projects/library/security/jwt/JwtAuthenticationFilter.java
@@ -70,3 +70,53 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         filterChain.doFilter(request, response);
     }
 }
+
+
+
+
+/*
+
+This code appears to be a filter that intercepts incoming HTTP requests to check for JWT-based authentication in a Spring Security context. Let's go through it step by step:
+
+protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {: This method overrides the doFilterInternal method of a javax.servlet.Filter class, indicating that it intercepts incoming HTTP requests.
+
+String token = null; String userName = null;: Initializes variables token and userName to null.
+
+String authHeader = request.getHeader("Authorization");: 
+Retrieves the "Authorization" header from the incoming HTTP request.
+
+if(authHeader != null && authHeader.startsWith("Bearer ")) {:
+Checks if the Authorization header is not null and starts with "Bearer ", indicating it's a Bearer token (JWT).
+
+token = authHeader.substring(7);:
+Extracts the token string by removing the "Bearer " prefix from the Authorization header.
+
+userName = jwtService.extractUsernameFromToken(token);: 
+Uses a jwtService (presumably a service class) to extract the username from the JWT token.
+
+if(userName != null && SecurityContextHolder.getContext().getAuthentication() == null) {: 
+Checks if the username is not null and if there's no existing authentication in the SecurityContextHolder.
+
+UserDetails userDetails = userDetailsService.loadUserByUsername(userName);:
+Loads user details (presumably from a userDetailsService) using the extracted username.
+
+var authToken = new UsernamePasswordAuthenticationToken(token, null, userDetails.getAuthorities());:
+Creates an authentication token (authToken) of type UsernamePasswordAuthenticationToken using the token string, null credentials, and the authorities loaded from the user details.
+
+authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));:
+Sets additional details for the authentication token, such as the source of authentication details from the incoming request.
+
+SecurityContextHolder.getContext().setAuthentication(authToken);:
+Sets the authentication token in the SecurityContextHolder.
+
+filterChain.doFilter(request, response);:
+Passes the request and response objects along the filter chain for further processing.
+
+Overall, this filter intercepts requests, checks for a Bearer token in the Authorization header, 
+extracts the username from the token, loads user details, creates an authentication token,
+and sets it in the SecurityContextHolder for further processing of the request within the security context.
+
+
+
+
+*/
